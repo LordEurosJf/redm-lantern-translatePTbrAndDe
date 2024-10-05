95f0c7ae3c78207ee5bba508d579adb919d2d663
Edimilson Junior <160673897+LordEurosJf@users.noreply.github.com>
2024-10-05 14:55:27 -0300

    Add files via upload

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales = Locales or {}
+Locales['de'] = {
+    cooldown_active = "Bitte warten Sie, bevor Sie es erneut versuchen",
+    horse_not_found = "Pferd nicht gefunden.",
+    horse_too_far = "Dein Pferd ist zu weit von dir entfernt.",
+    lantern_unequipped = "Laterne vom Pferd entfernt.",
+    no_lantern = "Du hast kein Seitenlicht.",
+    lantern_equipped = "Laterne am Pferd angebracht.",
+    lantern_interaction = "Taschenlampe",
+}
---
+++ locales/pt-BR.lua
@@ -0,0 +1,10 @@
+Locales = Locales or {}
+Locales['pt-BR'] = {
+    cooldown_active = "Aguarde para tentar novamente",
+    horse_not_found = "Cavalo nao encontrado.",
+    horse_too_far = "Seu cavalo esta muito longe de vc.",
+    lantern_unequipped = "Lanterna removida do cavalo.",
+    no_lantern = "Voce nao tem uma laterna.",
+    lantern_equipped = "Lanterna equipada no pocoto.",
+    lantern_interaction = "Lanterna",
+}
